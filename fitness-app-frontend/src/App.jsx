c916f020ee12dbeda56c4a33b0f54faf7369ae08
Suryansh441 <suryanshmakhloga441@gmail.com>
2026-06-01 21:24:22 +0530

    included config server

---
+++ fitness-app-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "react-bootstrap";
+import { BrowserRouter as Router, Navigate, Route, Routes, useLocation} from "react-router";
+
+function App() {
+  
+
+  return (
+    <Router>
+      <Button></Button>
+    </Router>
+  )
+}
+
+export default App
